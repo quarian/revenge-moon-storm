9280adf5173886ab5cc95f727a95c749d5b70dec
lippinj1-aalto.fi <lippinj1-aalto.fi@66cd5110-f5de-4c6e-a408-ced7c5b16b3e>
2013-11-06 22:58:22 +0000

    Fleshed out the code for Walkers and defined the header more fully.

--- src/Walker.hh
+++ src/Walker.hh
@@ -43,8 +43,77 @@ public:
 
 
 private:
-    const Map& map_;
+    /* Movement "in-between" two blocks is understood to happen in such a way
+     * that, when the Walker is NOT centered (standing still at the center of
+     * its current MapBlock) it will have a distance-from-center in EITHER
+     * the x OR the y direction, represented by dx_ and dy_:
+     *
+     *  (-0.5, -0.5)                   (0.5, -0.5)
+     *      #################################     -
+     *      #               |               #      |
+     *      #               |               #      |  negative
+     *      #               |               #      |    dy_
+     *      #               |               #      |
+     *      #               |               #      |
+     *      #         (0, 0)|      -->      #      |
+     *      #---------------+------W--------#     -
+     *      #               |               #      |
+     *      #               |               #      |
+     *      #               |               #      |  positive
+     *      #               |               #      |    dy_
+     *      #               |               #      |
+     *      #               |               #      |
+     *      #################################     -
+     *  (-0.5, 0.5)                    (0.5, 0.5)
+     *
+     *      '_______________'_______________'
+     *
+     *          negative        positive
+     *            dx_             dx_
+     *
+     * In the figure above, the Walker W is moving towards the right ("EAST"),
+     * incrementing its dx_ by an appropriate amount each turn. The Walker
+     * keeps track of whether it is moving towards or away from the center,
+     * and increments or decrements its coordinates with approach(dt) or
+     * depart(dt), whichever is appropriate.
+     *
+     * When the Walker reaches the center of its MapBlock, it "knocks" on the
+     * next MapBlock (unless ordered to stop moving). knock() determines
+     * whether it is possible to move to the next MapBlock, mine it, or do
+     * some other operation on it. Similarly, when an EDGE is reached (some
+     * coordinate reaches magnitude 0.5), the Walker attempts to move into
+     * the next MapBlock. 
+     */
+
+    /* Moves towards the center of the current block, according to the given
+     * delta-time. This function assumes that the dt given is less than or
+     * equal to the time required to get to the center of the block, that is,
+     * it assumes that the progress will not lead to the Walker actually
+     * reaching the center. */
+    void approach(float);
+
+    /* Moves away from the center of the current block, according to the given
+     * delta-time. This assumes that the dt is not large enough for the Walker
+     * to actually leave the square. */
+    void depart(float);
+
+    /* Moves to the center of the block. */
+    void center();
+
+    /* Tries to place the Walker at the appropriate edge of the "next"
+     * MapBlock. */
+    void proceed();
+
+    /* Starts mowing into the next square in the currently facing direction.
+     * Inheriting classes that can dig etc. should override this! */
+    virtual void knock(float);
+
+    /* Returns true if the Walker is currently at the center of its block. */
+    bool centered() const { return dx_ == 0 && dy_ == 0; }
+
+    Map& map_;
     MapBlock* location_;
+    MapBlock* target_;
     Direction dir_;
 
     bool entering_; // Is this Walker entering or leaving the square?
