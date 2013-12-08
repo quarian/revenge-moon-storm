94c4d4b0ee29ec04aa9a1092c0aba8f56d4305c8
lippinj1-aalto.fi <lippinj1-aalto.fi@66cd5110-f5de-4c6e-a408-ced7c5b16b3e>
2013-12-08 04:28:55 +0000

    MapObjectManager created

---
+++ src/MapObjectManager.cc
@@ -0,0 +1,23 @@
+#include "MapObjectManager.hh"
+#include "Map.hh"
+#include "MapBlock.hh"
+
+bool MapObjectManager::place(char signifier, Map& map, MapBlock* mb) {
+    switch (signifier) {
+        /* MONSTERS */
+        case 'x': {
+            auto bug = new TinyBug(map, mb);
+            map.monsters.insert(bug);
+            mb->enter(bug);
+            return true;
+        }
+
+        /* TREASURES */
+
+
+        /* DEFAULT */
+        default: {
+            return false;
+        }
+    }
+}
---
+++ src/MapObjectManager.hh
@@ -0,0 +1,14 @@
+#ifndef MB2_MAPOBJECTMANAGER_HH
+#define MB2_MAPOBJECTMANAGER_HH
+
+#include "Item.hh"
+#include "Enemies.hh"
+
+class Map;
+class MapBlock;
+
+namespace MapObjectManager {
+    bool place(char, Map&, MapBlock*);
+}
+
+#endif
